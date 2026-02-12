078ad4cb6d64afb0ce46c3ded7f8bdb68b8a8b30
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-12 12:54:36 -0500

    part 1

--- Topic4Assignment/Program.cs
+++ Topic4Assignment/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Topic4Assignment
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Topic4Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name;
+            int age;
+            double price;
+            Console.WriteLine("Hello there! What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("It's a pleasure to meet you, " + name + ", what about your age?");
+            Console.WriteLine (Int32.TryParse (Console.ReadLine(), out age));
+            Console.WriteLine("Well that's interesting! " + age + ", huh? And what about how much you make?");
+            double.TryParse(Console.ReadLine(), out price);
+            Console.WriteLine(price.ToString("C") + "? Sure hope that's by the hour and not weekly!");
+
         }
     }
 }
