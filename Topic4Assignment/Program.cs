ef78a06da1e0503feff0a853818e4a782235f261
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-13 13:09:21 -0500

    cleanup

--- Topic4Assignment/Program.cs
+++ Topic4Assignment/Program.cs
@@ -17,12 +17,15 @@ namespace Topic4Assignment
             Console.WriteLine("Hello there! What's your name?");
             name = Console.ReadLine();
             Console.WriteLine("It's a pleasure to meet you, " + name + ", what about your age?");
-            Console.WriteLine (Int32.TryParse (Console.ReadLine(), out age));
+            age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Well that's interesting! " + age + ", huh? And what about how much you make?");
             double.TryParse(Console.ReadLine(), out price);
             Console.WriteLine(price.ToString("C") + "? Sure hope that's by the hour and not weekly!");
-            Console.WriteLine("    ");
-          
+            Console.WriteLine("   ");
+            Console.WriteLine("\t\t Press SPACEBAR to continue \t ");
+            Console.ReadLine();
+            Console.Clear();
+           
             Console.WriteLine("Please enter the following information.");
             Console.Write("First name: ");
             name = Console.ReadLine();
@@ -42,8 +45,11 @@ namespace Topic4Assignment
             Console.WriteLine("Name:\t\t" + lastName + ", " + name);
             Console.WriteLine("Average:\t" + average + "%");
             Console.WriteLine("Grade:\t\t" + grade);
-            Console.WriteLine("    ");
-         
+            Console.WriteLine("   ");
+            Console.WriteLine("\t\t Press SPACEBAR to continue \t ");
+            Console.ReadLine();
+            Console.Clear();
+          
             Console.Write("What is your name? ");
             name = Console.ReadLine();
             Console.WriteLine("Hello there, " + name + ", how old are you?");
