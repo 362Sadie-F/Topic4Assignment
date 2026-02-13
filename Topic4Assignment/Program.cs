23e22c31bfc901e02b8e24223b2c7e8a90b21cd1
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-13 12:38:55 -0500

    part 2

--- Topic4Assignment/Program.cs
+++ Topic4Assignment/Program.cs
@@ -7,6 +7,11 @@ namespace Topic4Assignment
         static void Main(string[] args)
         {
             string name;
+            string lastName;
+            string login;
+            int studentID;
+            double average;
+            int grade;
             int age;
             double price;
             Console.WriteLine("Hello there! What's your name?");
@@ -16,7 +21,26 @@ namespace Topic4Assignment
             Console.WriteLine("Well that's interesting! " + age + ", huh? And what about how much you make?");
             double.TryParse(Console.ReadLine(), out price);
             Console.WriteLine(price.ToString("C") + "? Sure hope that's by the hour and not weekly!");
-
+            Console.WriteLine("    ");
+            Console.WriteLine("Please enter the following information.");
+            Console.Write("First name: ");
+            name = Console.ReadLine();
+            Console.Write("Last name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Grade [9-12]: " );
+            grade = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Student ID: ");
+            studentID = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Login: ");
+            login = Console.ReadLine();
+            Console.Write("Grade average: ");
+            double.TryParse (Console.ReadLine(), out average);
+            Console.WriteLine("Information display: ");
+            Console.WriteLine("Login:\t\t" + login);
+            Console.WriteLine("ID:\t\t" + studentID);
+            Console.WriteLine("Name:\t\t" + lastName + ", " + name);
+            Console.WriteLine("Average:\t" + average + "%");
+            Console.WriteLine("Grade:\t\t" + grade);
         }
     }
 }
